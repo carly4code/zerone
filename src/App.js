a2c1ea6f41212ef004bfa5c4dbd48637390feb6e
gabriel888chow <94506701+gabriel888chow@users.noreply.github.com>
2022-03-15 14:38:50 +0800

    commit to Gabriel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+// import Infinity from './Components/Infinity.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <svg viewBox= "-2000 -1000 4000 2000">
+        <path id="stroke" d="M354-354A500 500 0 1 1 354 354L 354L-354A500 500 0 1 0-354 354z"></path>
+        <use xlink:href="#stroke" stroke-dasharray="1570 5143" stroke-dashoffset= "6713px"></use>
+      </svg>
     </div>
   );
 }
