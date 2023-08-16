ef5f28c5dabc5c12fa9617b22ce14dd72b510306
carly4code <orkwan@hotmail.com.hk>
2023-08-16 12:40:00 +0800

    setup gh-pages

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -1,10 +1,15 @@
 import React, { Component, useState, useEffect } from 'react';
-import dummy_product from '../assets/images/shop/dummy-product.svg'
+import dummy_product from '../assets/images/shop/dummy-product.svg';
+import RedButton from '../elements/RedButton';
+import { Link } from 'react-router-dom';
+
 
 
 export default function Data() {
     const [useData, setUseData] = useState();
     const [isPopup, setIsPopup] = useState(false);
+    const togglePopup = () => {
+        setIsPopup(!isPopup);}
 
     const apiUrl = 'http://localhost:8000/Shop'
     let displayData
@@ -25,8 +30,7 @@ export default function Data() {
                         </div>
                         <p className='heading'>{title}</p>
                         <p className='subhead'>{overview}</p>
-                        <button>View Detail</button>
-
+                        <RedButton label='view'/>
                     </div>
                 </div>
             </div>
@@ -34,38 +38,42 @@ export default function Data() {
     };
 
     async function pullJson() {
+
         const response = await fetch(apiUrl)
         const responseData = await response.json()
-        console.log(responseData)
+       // console.log(responseData)
+
         displayData = responseData.map(function(todo) {
             return (
                 <div className="content-container product">
 
                     <div className="image-wrap">
-                    <a className="quick-view">
-                    <p>Quick view</p>
+                    <a className="quick-view"
+                    onClick={togglePopup}>
+                        <RedButton label='Quick View'/>
                     </a>
                     <img src={dummy_product} alt="coffee" />
                     </div>
+                    <Link to="/Product" style={{textAlign:'center', textDecoration:'none'}}>
                     <p key={todo.id}>{todo.name}</p>
                     <br/>
                     <p>{todo.title}</p>
-                    <p>{todo.price}</p>
-                    {/* <button>View Detail</button> */}
-                        <div className='coffee-card' onClick={() => {
-                                setIsPopup(!isPopup);
-                            }}>
+                    <br/>
+                    {todo.salePrice
+                    ? <><p style={{color: "#CC2840"}}>{todo.salePrice}</p>
+                    <p style={{textDecoration: "line-through"}}>{todo.price}</p></>
+                    : <p>{todo.price}</p>}
+                    </Link>
+                        <div className='coffee-card'>
                                 <CoffeePop />
                                 <div>
-                                {/* <a className="quick-view">
-                                    <p>Quick view</p>
-                                </a> */}
+
                         </div>
                     </div>
                 </div>
             )
         })
-        console.log(responseData)
+  //      console.log(responseData)
         setUseData(displayData)
 
     }
@@ -77,8 +85,12 @@ export default function Data() {
     return (
         <div>
            <section className='product-bar'>
+          <div className='container' style={{
+                        padding: "0 30px",
+                    }}>
                 <div className="product-row">
                     {useData}
+                </div>
                 </div>
             </section>
         </div>
